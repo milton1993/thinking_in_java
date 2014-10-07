c1b2b132c82c0cfb375210140f93ab1823b6885a
milton1993 <mitong@aliyun.com>
2014-10-07 21:34:26 +0800

    ShiftTest.java modified.

--- ShiftTest.java
+++ ShiftTest.java
@@ -8,6 +8,6 @@ public class ShiftTest {
 		long c = 100L;
 		long d = c << 100; //其实是移动了36位，只考虑100的低6位
 		String cBinary = Long.toBinaryString(d);
-		System.out.println("对a的移位运算移动了" + (cBinary.length() - cBinary.indexOf("1100100") - 7) + "位");
+		System.out.println("对c的移位运算移动了" + (cBinary.length() - cBinary.indexOf("1100100") - 7) + "位");
 	}
 }
